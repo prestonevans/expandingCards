b4ba462d91979b591a92a64bd7d58dfa718998ee
prestonevans <89256614+prestonevans@users.noreply.github.com>
2022-08-03 18:40:08 -0600

    auto play responsive photo gallery

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
-//document.body.style.backgroundColor = "red";
-//test me
+const images = document.querySelectorAll('img');
+let intervalId;
+
+galleryAutoPlay()
+
+images.forEach((image) => {
+  image.addEventListener('click', () => {
+    clearInterval(intervalId);
+    galleryAutoPlay()
+    images.forEach((image) => image.classList.remove('active'));
+    image.classList.add('active');
+  });
+});
+
+function galleryAutoPlay() {
+  intervalId = setInterval(() => {
+    let activeIndex;
+    images.forEach((image, index) => {
+      if (image.classList.contains('active')) {
+        activeIndex = index;
+        image.classList.remove('active');
+      }
+    });
+    if (activeIndex + 1 === images.length) {
+      images[0].classList.add('active');
+      return;
+    }
+    images[activeIndex + 1].classList.add('active');
+  }, 4 * 1000);
+}
